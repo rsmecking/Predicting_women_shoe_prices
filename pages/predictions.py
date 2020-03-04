f28ff61775d369ed62a148b25774ebb23633d024
Ryan Mecking <50974677+rsmecking@users.noreply.github.com>
2020-03-04 12:33:13 -0500

    Update predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -12,7 +12,7 @@ from app import app
 
 
 #Pipeline
-pipeline = load('assets/pipeline.joblib3_test2')
+pipeline = load('assets/pipeline.joblib3_test1')
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -29,17 +29,14 @@ column1 = dbc.Col(
         ),
 
         # dcc.Checklist(
+        #     id='checklist', 
         #         options = [
-        #             # {'label': 'Open Toe', 'value': 'toe'},
-        #             # {'label': 'Ankle Cut', 'value': 'ankle'},
-        #             # {'label': 'Has Heel', 'value': 'heel'},
-        #             # {'label': 'Other', 'value': 'other'},
-        #             {'label': 'On Sale', 'value': 'prices.isSale'},
+        #              {'label': 'On Sale', 'value': 'HasSale'},
         #         ],
-        #         labelStyle={'display': 'inline-block'}
+        #         labelStyle={'display': 'inline-block'},
+        #         value = None
         #  ), 
-
-
+                 
         dcc.Dropdown(
             id='brand', 
                 options = [
@@ -51,7 +48,7 @@ column1 = dbc.Col(
                 ], 
                 placeholder="Select a Brand",
                 value = 'Brand', 
-                className='mb-4',
+                className='mb-2',
          ),   
 
         dcc.Dropdown(
@@ -80,9 +77,55 @@ column1 = dbc.Col(
                 ], 
                 placeholder="Select a Manufacturer(Top 20)",
                 value = 'Manufacturer', 
-                className='mb-4',
+                className='mb-2',
          ), 
+
+        html.Div([
+			    dcc.Markdown("Claimed to be on sale"),
+			    dcc.RadioItems(
+                    id='HasSale',
+                    options=[
+                        {'label': 'Yes', 'value': '0'},
+                        {'label': 'No', 'value': '1'}],
+                    value='1',
+                    labelStyle={'display': 'inline-block'}
+		    )
+		]),
     
+        # dcc.Dropdown(
+        #      id='name',
+        #         options=[
+        #             {'label': 'Open Toe', 'value': 'toe'},
+        #             {'label': 'Ankle High', 'value': 'ankle'},
+        #             {'label': 'Has Heel', 'value': 'heel'},
+        #             {'label': 'Sandal', 'value': 'sandal'},
+        #             {'label': 'Straps', 'value': 'strap'},
+        #             {'label': 'Leather', 'value': 'leather'},
+        #             {'label': 'Fashion', 'value': 'fashion'},
+        #             {'label': 'Open', 'value': 'open'},
+        #             {'label': 'Pump', 'value': 'pump'},
+        #             {'label': 'Suede', 'value': 'suede'},
+        #             {'label': 'Wide', 'value': 'wide'},
+        #             {'label': 'Lace', 'value': 'lace'},
+        #             {'label': 'Slip on', 'value': 'slip'},
+        #             {'label': 'Calf High', 'value': 'calf'},
+        #             {'label': 'Flat', 'value': 'flat'},
+        #             {'label': 'Riding', 'value': 'riding'},
+        #             {'label': 'Step', 'value': 'step'},
+        #             {'label': 'Wedge', 'value': 'wedge'},
+        #             {'label': 'Dress', 'value': 'dress'},
+        #             {'label': 'Running', 'value': 'running'},
+        #             {'label': 'Knee High', 'value': 'knee'},
+        #             {'label': 'Slingback', 'value': 'slingback'},
+        #             {'label': 'Zipper', 'value': 'zipper'}
+        #         ],
+        #         placeholder='Select all that apply',
+        #         multi=True,
+        #         value = 'Description',
+        #         className='mb-2',
+        #  ),  
+
+
     ],
     md=4,
 )
@@ -95,13 +138,16 @@ column2 = dbc.Col(
 )
 
 @app.callback(
-    Output('prediction-content', 'children'),
-    [Input('brand', 'value'), Input('manufacturer', 'value')],
+    Output('prediction-content', 'children'),[
+    Input('brand', 'value'), 
+    Input('manufacturer', 'value'), 
+    Input('HasSale', 'value'),
+    ],
 )
-def predict(brand, manufacturer):
+def predict(brand, manufacturer, HasSale):
     df = pd.DataFrame(
-        columns=['brand', 'manufacturer'], 
-        data=[[brand, manufacturer]]
+        columns=['brand', 'manufacturer', 'HasSale'], 
+        data=[[brand, manufacturer, HasSale]]
     )
     y_pred = pipeline.predict(df)[0]
     return f'estimated price ${y_pred:.02f} '
