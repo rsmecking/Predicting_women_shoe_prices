d918ef4c314641419df58858c1b23c2391c4bbbb
Ryan Mecking <50974677+rsmecking@users.noreply.github.com>
2020-03-05 15:26:58 -0500

    Update process.py

--- pages/process.py
+++ pages/process.py
@@ -15,9 +15,28 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Process
+            #### Process
 
+            Much of the [data](https://www.kaggle.com/datafiniti/womens-shoes-prices#7210_1.csv)
+            was hardly usable for prediction purposus. I needed to do a lot of feature 
+            engineering to obtain any usable to get any predictability for the target, price. 
+            
+            
+            ##### Target
+            Below are all the prices(the target), of all the  shoes graphed. The graph in orange, is the prices logged. 
+            This allows the more extreme prices to be evenly "weighted" or drawn out like it is shown in the 
+            blue graph. 
 
+            ![](assets/Price_graphed.png)  ![](assets/Price_y_loged.png)            
+
+            ##### Machine Learning Models
+            I ran the data through a linear , random forest, and XGBoots regression ML pipelines.  
+            Every model was able to beat the baseline, infurring I was able to train the models correctly. Below 
+            is an graph showing a [validation curve](https://scikit-learn.org/stable/modules/learning_curve.html). 
+            This is used to find the best paramaters to obtain a good fitting model. 
+
+            ![](assets/XGB_Val_Curve.png)
+            
             """
         ),
 
