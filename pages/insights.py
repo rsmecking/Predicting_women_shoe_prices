6ef4d9299cffbc5e8cad1e8e3b8b284b00db11bf
Ryan Mecking <50974677+rsmecking@users.noreply.github.com>
2020-03-05 15:26:26 -0500

    Update insights.py

--- pages/insights.py
+++ pages/insights.py
@@ -15,8 +15,27 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Insights
+            ### Insights
 
+           For usability in the app, I only took the top 12 most practical features from 
+           a process called permutation importance. For readability I created a eli5 chart shown below. 
+           The features were designed to incorporate a list of descriptive words and create binary 
+           columns for the learning models to interpret.  For example, 'has_heel' feature is any shoe 
+           that may have a heel.  Though this isn't very specific, you can see below, heels do not play
+           a large role in determining shoe price.
+
+
+           ![](assets/feature_importances.png)   
+
+           #### SHAP 
+           SHAP is a need little visualation to show how the features above effect the price of the shoe. The actual
+           price of the shoe below is $33.99 and the model predicted it to be $36.29. Though off by $2.30, it still
+           gives a fairly accurate representation of what that shoe cost is.
+           
+           ![](assets/Shap_graph.png) 
+
+
+           
 
             """
         ),
