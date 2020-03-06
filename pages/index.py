a989880c91ce528dd875d13baf2d67c1233db32d
Ryan Mecking <50974677+rsmecking@users.noreply.github.com>
2020-03-05 21:16:33 -0500

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -18,17 +18,19 @@ column1 = dbc.Col(
         
             ## Shoe Price Prediction
 
-            🦶My passion for shoes is lackluster at best. I couldn't tell
-            you shoe price if my life depended on it.
+            🦶My passion for shoes is lackluster at best. I couldn't tell you shoe 
+            prices if my life depended on it.
 
-            👢This has really gone and bite me in the butt a few times.
-            My wife has a health appetite for shoes and is quick to destroy any
-            evidence linking cost to the shoes. 
+
+            👢This has really gone and bit me in the butt a few times. My wife has a 
+            healthy appetite for shoes and is quick to destroy any evidence linking 
+            cost to the shoes. I have to take her word that it cost a certain amount 
+            because it was on sale and clearance.
     
 
-            👠This app is built specifically for individuals who may lack shoe 
-            knowledge that may be curious to roughly how expensive, specifically 
-            women shoes, given certain descriptive words.
+            👠This app is built specifically for individuals who may lack shoe knowledge, 
+            and may be curious to roughly how expensive, specifically women shoes are, 
+            given certain parameters.
 
             """
         ),
